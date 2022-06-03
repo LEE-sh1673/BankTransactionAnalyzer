7a533ea3265257223587a0a1eedf49f5d99e4e9c
LEE-sh1673 <lsh901673@gmail.com>
2022-06-03 17:32:55 +0900

    Modularize `getMonthTransactions` in `BankTransactionAnalyzer.java`.

--- src/main/java/org/lee_sh1673/BankTransactionAnalyzer.java
+++ src/main/java/org/lee_sh1673/BankTransactionAnalyzer.java
@@ -4,53 +4,53 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.time.Month;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankTransactionAnalyzer {
     private static final String RESOURCES = "src/main/resources/";
 
-    // TODO: 현재 코드는 특정 데이터 형식 만을 지원함. 새로운 열을 추가하거나 다른 데이터 형식을 지원하도록 구현할 것.
-    public static void getTotalTransactions(final String file) throws IOException {
-
-        // TODO: `getMonthTransactions`와 중복된 부분 발생. 추후 리펙토링 필요.
-        final Path path = Paths.get(RESOURCES + file);
-        final List<String> lines = Files.readAllLines(path);
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
         double total = 0d;
 
-        for (final String line : lines) {
-            final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
         }
-        System.out.println("The total for all transactions is " + total);
+        return total;
     }
 
-    // TODO: 현재 코드는 특정 데이터 형식 만을 지원함. 새로운 열을 추가하거나 다른 데이터 형식을 지원하도록 구현할 것.
-    public static void getMonthTransactions(final String file,
-                                            final Month month) throws IOException {
+    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions,
+                                                      final Month month) {
 
-        // TODO: `getTotalTransactions`와 중복된 부분 발생. 추후 리펙토링 필요.
-        final Path path = Paths.get(RESOURCES + file);
-        final List<String> lines = Files.readAllLines(path);
-        double total = 0d;
-        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
 
-        for (final String line : lines) {
-            final String[] columns = line.split(",");
-            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
-
-            if (date.getMonth() == month) {
-                final double amount = Double.parseDouble(columns[1]);
-                total += amount;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                bankTransactionsInMonth.add(bankTransaction);
             }
         }
-        System.out.println("The total for all transactions in " + month.name() + " is " + total);
+        return bankTransactionsInMonth;
     }
 
-    public static void main(String[] args) throws IOException {
+    // TODO: 현재 코드는 특정 데이터 형식 만을 지원함. 새로운 열을 추가하거나 다른 데이터 형식을 지원하도록 구현할 것.
+    public static void getMonthTransactions(final String fileName,
+                                            final Month month) throws IOException {
 
+        final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
+
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions
+                = bankStatementParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transactions in " + month.name() + " " + selectInMonth(bankTransactions, month));
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        getMonthTransactions(args[0], Month.JANUARY);
     }
 }
