15a28631e7646d8189ce95c432efddf16f3c9dcc
LEE-sh1673 <lsh901673@gmail.com>
2022-06-04 17:19:26 +0900

    Create DAO class of for CRUD of `BankTransaction` objects.

---
+++ src/main/java/org/lee_sh1673/BankTransactionDAO.java
@@ -0,0 +1,28 @@
+package org.lee_sh1673;
+
+import java.time.LocalDate;
+
+public class BankTransactionDAO {
+    public BankTransaction create(final LocalDate date,
+                                  final double amount,
+                                  final String description) {
+
+        /* TODO: implement later. */
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction read(final long id) {
+        /* TODO: implement later. */
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction update(final long id) {
+        /* TODO: implement later. */
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction delete(final BankTransaction bankTransaction) {
+        /* TODO: implement later. */
+        throw new UnsupportedOperationException();
+    }
+}
