4533f01f8f75c425b5addbbc559bb6c7a23be4a2
LEE-sh1673 <lsh901673@gmail.com>
2022-06-04 18:31:22 +0900

    Add test class `BankStatementCSVParserTest.java` for test parsing data.

---
+++ src/test/java/org/lee_sh1673/BankStatementCSVParserTest.java
@@ -0,0 +1,33 @@
+package org.lee_sh1673;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class BankStatementCSVParserTest {
+
+    private final BankStatementParser statementParser
+            = new BankStatementCSVParser();
+
+    @Test
+    public void shouldParseOneCorrectLine() {
+        // Set up test context.
+        final String line = "30-01-2017,-50,Tesco";
+
+        // Execute parsing test.
+        final BankTransaction result = statementParser.parseFrom(line);
+        final BankTransaction expected = new BankTransaction(
+                LocalDate.of(2017, Month.JANUARY, 30),
+                -50,
+                "Tesco"
+        );
+        final double tolerance = 0.0d;
+        
+        // Set up assertions of expected results. -> Assert.assertEquals(expected, actual, [delta]);
+        Assert.assertEquals(expected.getDate(), result.getDate());
+        Assert.assertEquals(expected.getAmount(), result.getAmount(), tolerance);
+        Assert.assertEquals(expected.getDescription(), result.getDescription());
+    }
+}
